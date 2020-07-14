d0b3766acc9d77d1294f5d6d096fd5a05e88d0cf
mattfan00 <kungfumo@gmail.com>
2020-07-13 23:01:38 -0400

    finished first version of add

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 let keys = {};
 
-
 document.addEventListener('keydown', (e) => {
   if (!(e.key in keys)) {
-    return;
+    return true;
   }
   
-  console.log(keys);
-  console.log('detected key down');
+  // console.log(keys);
+  
+  let key = keys[e.key]; 
+  
+  if (!key.pressed) {
+    key.pressed = true;
+    key.action();
 
-
+    key.interval = setInterval(() => {
+      key.action();
+    }, key.time)
+  }
+  return false;
 })
 
 document.addEventListener('keyup', (e) => {
-  console.log('key up');
+  if (e.key in keys) {
+    let key = keys[e.key];
+
+    if (key.interval !== null) {
+      clearInterval(key.interval);
+      key.interval = null;
+      key.pressed = false;
+    }
+  }
 })
 
 let hold = {
   add: function(key, time, action) {
     keys[key] = {
       time,
-      action
+      action,
+      interval: null,
+      pressed: false
     };
     console.log(keys);
   },
